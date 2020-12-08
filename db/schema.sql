97f172589ee453fa5cb0d1dc80c5183e732d600f
Morgan <morganw118@gmail.com>
2020-12-07 16:56:03 -0800

    created initial schema for sql db

--- db/schema.sql
+++ db/schema.sql
@@ -7,10 +7,10 @@ USE employee_db;
 
 CREATE TABLE employee (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id VARCHAR(30) NOT NULL,
-  manager_id INTEGER NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id VARCHAR(30),
+  manager_id INTEGER(10) NULL,
   PRIMARY KEY (id)
 );
 
@@ -18,3 +18,16 @@ DROP TABLE songs;
 
 INSERT INTO songs (title, artist, genre)
 VALUES ("Solo", "Griz", "EDM");
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30)
+);
